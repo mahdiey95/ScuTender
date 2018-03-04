e73dafad489dd54f37f8a95aa483f47a620bc846
Haniye <hnysrz261173@gmail.com>
2018-03-04 23:02:48 +0330

    titels

--- resources/views/contractor_detail.blade.php
+++ resources/views/contractor_detail.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Business clean web templates</title>
+    <title>سامانه مناقصات دانشگاه شهید چمران اهواز</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
