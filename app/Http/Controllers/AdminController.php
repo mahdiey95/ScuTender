707d0fad2a47ac91f07065b750efa0c7f28a7a1e
Mahdi <mahdi.ey95@gmail.com>
2018-03-05 21:57:25 +0330

    tenders now have 4 status levels sysadmin can now add admins and experts to system

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['index']]);
+    }
+
+
+
+
+    public function storeAdmin(Request $request)
+    {
+        if(Auth::user()->role != 'SYSADMIN')
+            return abort(403);
+
+
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:3|confirmed'
+        ]);
+
+        $user = new User([
+            'name' => $data['name'],
+            'username' => $data['username'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        if($user->save())
+            return back()->with('success','کاربر ثبت شد');
+        else
+            return abort(500);
+
+    }
+
+    public function createAdmin() {
+        if(Auth::user()->role != 'SYSADMIN')
+            return abort(403);
+
+        return view('auth.register_admin');
+    }
+}
